4908298046167ddc773b75cf91cb0567831f38c7
Joshua Chen <jzc0289@auburn.edu>
2024-09-05 08:51:57 -0500

    add test GUI windows using oracle docs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
 
+public class Main {
+    /**
+     * Creates GUI window and adds it various components
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    /**
+     * Calls the function to create window
+     *
+     * @param args arguments passed into program form the command line
+     */
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(Main::createAndShowGUI);
     }
 }

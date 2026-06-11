6fae60c675fabb1e4aea64df0228cb02d15679c4
Joshua Chen <jzc0289@auburn.edu>
2026-06-10 23:35:56 -0500

    feat(api): implement db test and getAllRecipes function

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,9 +1,23 @@
 import * as express from 'express';
 
+import RecipeService from './RecipeService';
+
 const router = express.Router();
 
 router.get('/api/hello', (req, res, next) => {
     res.json('World');
+});
+
+router.get("/api/dbTest", async (req, res, next) => {
+    const isAvailable: boolean = await RecipeService.TestDBConnection();
+
+    res.json(isAvailable);
+});
+
+router.get('/api/getAllRecipes', async(req, res, next) => {
+    const data = await RecipeService.getAllRecipes();
+    
+    res.json(data)
 });
 
 export default router;

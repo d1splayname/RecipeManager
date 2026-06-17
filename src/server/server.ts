de7b3cd1f153ff2d624e1a4ce7e22be29654ae58
Joshua Chen <jzc0289@auburn.edu>
2026-06-16 20:03:58 -0500

    feat(server): update port

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(apiRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));

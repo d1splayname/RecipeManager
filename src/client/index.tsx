e0ee6fc4f28c2f6f81a5343c87c773605b6f53dc
Joshua Chen <jzc0289@auburn.edu>
2026-06-16 23:53:32 -0500

    fix(client): mark root as always existing

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client'
-import App from './App';
+import App from './App.tsx';
 import './scss/app.scss';
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root')!);
 root.render(<App />);

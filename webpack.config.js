0d1d8d0257a5819043041df0ea3daa39e4a3c50c
Joshua Chen <jzc0289@auburn.edu>
2026-06-18 18:52:45 -0500

    fix(config): add back url path

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ const clientConfig = {
 	output: {
 		filename: 'app.js',
 		path: path.resolve(__dirname, 'public/js'),
-		publicPath: BASE_PATH + "/"
+		publicPath: process.env.NODE_ENV === 'production' ? '/recipes/' : '/'
 	}
 };
 

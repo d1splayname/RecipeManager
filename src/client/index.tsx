cab45e8f803f1b88ddbaa941db5329fc1a602efd
Joshua Chen <jzc0289@auburn.edu>
2026-06-18 17:24:57 -0500

    fix(client): fix import App.tsx error

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx';
+import App from './App';
 import './scss/app.scss';
 
 const root = createRoot(document.getElementById('root')!);
